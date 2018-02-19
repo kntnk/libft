f43bf4841fd8c0d474c9df59168f2d035d560fc6
Kentaro Tanaka <ktanaka@e1z2r5p30.42.us.org>
2018-02-19 10:30:17 -0800

    libft: Fixed errors

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ktanaka <ktanaka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 12:31:17 by ktanaka           #+#    #+#             */
-/*   Updated: 2018/02/18 20:41:02 by ktanaka          ###   ########.fr       */
+/*   Updated: 2018/02/19 10:23:59 by ktanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,26 @@
 
 char	*ft_strtrim(char const *s)
 {
-	size_t	i;
-	size_t	j;
+	int		i;
+	int		len;
+	char	*str;
 
-	i = 0;
-	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
-		i++;
-	if (s[i] == '\0')
-		return (ft_strdup(s + i));
-	j = ft_strlen(s) - 1;
-	while (s[j] == ' ' || s[j] == '\t' || s[j] == '\n')
-		j--;
-	return (ft_strsub(s, i, j - i + 1));
+	if (s == NULL)
+		return (NULL);
+	len = ft_strlen(s);
+	while (ft_isspace(s[len - 1]))
+		len--;
+	i = -1;
+	while (ft_isspace(s[++i]))
+		len--;
+	if (len <= 0)
+		len = 0;
+	if ((str = (char*)malloc(sizeof(char) * (len + 1))) == NULL)
+		return (NULL);
+	s += i;
+	i = -1;
+	while (++i < len)
+		str[i] = *s++;
+	str[i] = '\0';
+	return (str);
 }
