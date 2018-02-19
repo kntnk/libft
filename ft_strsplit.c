261a16a742be31bb269ac8e25afe59176fca7bba
Kentaro Tanaka <ktanaka@e1z2r5p30.42.us.org>
2018-02-18 20:25:57 -0800

    libft: Added extra function

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,50 +6,11 @@
 /*   By: ktanaka <ktanaka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 12:42:36 by ktanaka           #+#    #+#             */
-/*   Updated: 2018/01/26 16:02:56 by ktanaka          ###   ########.fr       */
+/*   Updated: 2018/02/18 20:23:07 by ktanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int		ft_nbstr_c(char const *s, char c)
-{
-	int		i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		while ((s[i] != '\0' && s[i] == c) || (s[i] != '\0' && s[i] != c))
-			i++;
-		if (s[i] != '\0')
-			i++;
-	}
-	return (i);
-}
-
-int		ft_strlen_c(char const *s, char c)
-{
-	int		i;
-
-	i = 0;
-	while (s[i] != '\0' && s[i] != c)
-		i++;
-	return (i);
-}
-
-int		ft_strcpy_c(char *dest, char const *src, char c)
-{
-	int		i;
-
-	i = 0;
-	while (src[i] != '\0' && src[i] != c)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (i);
-}
 
 char	**ft_strsplit(char const *s, char c)
 {
